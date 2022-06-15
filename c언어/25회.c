01f23b3c8d1b16715798c26b44db385905484c7a
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-06-15 19:24:30 +0900

    25,26

--- c언어/25회.c
+++ c언어/25회.c
@@ -1,1 +1,12 @@
-.
+#include<stdio.h>
+
+int main(void)
+{
+    int i;
+    int a[]={10,20,30,40,50,60,70,80,90,100};
+    int *ptr=a+3;
+    for(i=0;i<5;++i)
+    {
+        printf("%d\n",*(ptr+i)-3);
+    }
+}
