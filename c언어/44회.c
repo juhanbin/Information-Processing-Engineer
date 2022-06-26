33f287096a1ded3726e8f1f5be649e421d2c8f15
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-06-26 17:08:23 +0900

    43,44

---
+++ c언어/44회.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void change(int *px,int *py,int pc,int pd)
+{
+    *px=*py+pd;
+    *py=pc+pd;
+    pc=*px+pd;
+    pd=*px+py;
+}
+void main(void)
+{
+    int a=10,b=20,c=30,d=40;
+    change(&a,&b,c,d);
+    printf("a=%d,b=%d,c=%d,d=%d",a,b,c,d);
+}
