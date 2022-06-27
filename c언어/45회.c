686125f6ae5b273a4e2dac4f5b0205a654fb9ca4
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-06-27 14:20:02 +0900

    45,46

---
+++ c언어/45회.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+double h(double *f,int d,double x)
+{
+    int i;
+    double res=0.0;
+    for(i=d-1;i>=0;i--)
+    {
+        res=res * x+f[i];
+    }
+    return res;
+}
+int main()
+{
+    double f[]={1,2,3,4};
+    printf("%3.1f\n",h(f,4,2));
+    return 0;
+}
