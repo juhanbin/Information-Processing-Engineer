8789139b55e4d4ecc938b31ade23f6fc8e376f31
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-07-05 10:57:19 +0900

    56

---
+++ c언어/56회.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int my(int i,int j)
+{
+    if(i<3)
+        i=j=1;
+    else
+    {
+        i=i-1;
+        j=j-i;
+        printf("%d,%d,",i,j);
+        return my(i,j);
+    }
+}
+
+int main()
+{
+    my(5,14);
+    return 0;
+}
