95020bdde31faaa8515f794bd7e8f90d6b45f297
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-07-13 21:03:05 +0900

    1회

---
+++ java/1_1회.java
@@ -0,0 +1,8 @@
+public class Person {
+        public static void main(String[ ] args) {
+            Person p1=newPerson();//인스턴스 생성
+
+            p1.set_name("홍길동");
+            p1.set_age(40);
+        }
+}
---
+++ java/1회.java
@@ -0,0 +1,15 @@
+public class Person
+{
+    String name="홍길동";
+    int age=40;         //멤버변수 두개생성
+
+    public void setNmae(String n)
+    {
+        name=n;                     //메서드
+    }
+    public void setAge(int i)
+    {
+        age=1;
+    }
+}
+
