56e110203e4c3749f684a7ca0222073abc2e6a7a
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-07-16 21:37:19 +0900

    생성자, 예외처리

--- javaProject/study/src/Main.java
+++ javaProject/study/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args){
-        Phone Phone1=new Phone();//객체 생성
+        Phone Phone1 = new Phone("주판빈", 22, "red");//객체 생성
 
         System.out.println(Phone1.name);//이름출력
         System.out.println(Phone1.age);//나이출력
--- javaProject/study/src/Phone.java
+++ javaProject/study/src/Phone.java
@@ -1,11 +1,28 @@
 public class Phone {
-    String name="아이폰";      //이름속성
-    int age=23;             //나이속성
-    String color="red";     //색상속성
+    String name;      //이름속성
+    int age;             //나이속성
+    String color;     //색상속성
 
-    public void print(){    //메서드
+    public void print() {    //메서드
         System.out.println(name);
         System.out.println(age);
         System.out.println(color);
+
     }
+
+    public Phone(){
+
+    }
+    public Phone(String name, int age, String color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Phone(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+
 }
