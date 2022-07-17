98a4290b112576a853d1d5821f12465363fc52c4
juhanbin <89644904+juhanbin@users.noreply.github.com>
2022-07-17 22:47:42 +0900

    상속, 오버로딩, 오버라이딩

---
+++ javaProject/study/src/ClassP.java
@@ -0,0 +1,11 @@
+public class ClassP {
+    int func1(int a,int b){
+        return (a+b);
+    }
+    int func2(int a,int b) {
+        return (a - b);
+    }
+    int func3(int a,int b) {
+        return (a * b);
+    }
+}
--- javaProject/study/src/Main.java
+++ javaProject/study/src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
-        Phone Phone1 = new Phone("주판빈", 22, "red");//객체 생성
-
-        System.out.println(Phone1.name);//이름출력
-        System.out.println(Phone1.age);//나이출력
-        System.out.println(Phone1.color);//색상출력
-
-        Phone1.print();//메서드를 활용한 출드
-
-        Phone1.color="black";//Phone1객체의 색상을 검정색으로 바꿈
-        System.out.println(Phone1.color);//바뀐 Phone1의색을 출력
+        ClassP p=new ClassA();
+        System.out.println(p.func1(5,2)+","+p.func2(5,2)+","+p.func3(5,2));
     }
 }
 
